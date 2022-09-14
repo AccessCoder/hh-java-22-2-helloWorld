6d44b7d4b3b917bbb9ad6c08dc9e9a37b81821b6
Dominic <andro5@web.de>
2022-09-14 15:23:42 +0200

    Add DI

--- src/main/java/de/neuefische/hhjava222helloworld/controller/GreetingsController.java
+++ src/main/java/de/neuefische/hhjava222helloworld/controller/GreetingsController.java
@@ -1,32 +1,22 @@
 package de.neuefische.hhjava222helloworld.controller;
 
+import de.neuefische.hhjava222helloworld.service.GreetingService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingsController {
 
-    String name = "";
 
-    @GetMapping
-    public String sayHello(){
-        return "Wazzuuup! " + name + "?";
+    private final GreetingService service;
+
+    public GreetingsController(GreetingService service) {
+        this.service = service;
     }
 
-    @GetMapping("/en")
-    public String sayHello2(){
-        return "Hellooooo";
-    }
-
-    @GetMapping(path = "{name}")
-    public String sayHello3(@PathVariable String name, @RequestParam String q){
-        return q + " " + name;
-    }
-
-    @PostMapping
-    public String postName(@RequestBody String newName){
-        name = newName;
-        return "Name was updated to " + name;
+    @GetMapping(path = "{id}")
+    public String sayHello(@PathVariable String id){
+        return service.getGreetingById(id);
     }
 
 
---
+++ src/test/java/de/neuefische/hhjava222helloworld/service/GreetingServiceTest.java
@@ -0,0 +1,13 @@
+package de.neuefische.hhjava222helloworld.service;
+
+
+import de.neuefische.hhjava222helloworld.repository.GreetingsRepo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class GreetingServiceTest {
+
+}
